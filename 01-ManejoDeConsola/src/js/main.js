95896b6001201cbb03111f6515aadf275b5b7539
SamuVD <samuvv2905@gmail.com>
2024-04-15 09:33:17 -0500

    Mera introducción

---
+++ 01-ManejoDeConsola/src/js/main.js
@@ -0,0 +1,39 @@
+let userName = prompt ("Enter your user name:")
+let lastName = prompt ("Enter your last name:")
+const age = prompt ("Enter your age:")
+const email = prompt ("Enter your email:")
+let address = prompt ("Enter your address:")
+let phone = prompt ("Enter your phone:")
+
+list = [userName , lastName , age , email , address , phone]
+console.table (list)
+/*console.log ("Name: "+userName)
+console.error ("Last Name: "+lastName)
+console.warn ("Age: "+age)
+console.debug ("Email: "+email)
+console.info ("Address: "+address)
+console.log ("Phone: "+phone)*/
+
+// console.log (`Name: ${userName} - Last Name: ${lastName} - Age: ${age} - Email: ${email} - Address: ${address} - Phone: ${phone}.`)
+// console.error (userName +" "+ lastName +" "+ age +" "+ email +" "+ address +" "+ phone)
+// console.warn (userName , lastName , age , email , address , phone)
+
+// console.group ("Data basic information")
+
+// console.log ("Name: "+userName)
+// console.error ("Last Name: "+lastName)
+// console.warn ("Age: "+age)
+
+// console.groupEnd ()
+
+// console.group ("contact information")
+
+// console.log ("Email: "+email)
+// console.error ("Phone: "+phone)
+// console.warn ("Address: "+address)
+
+// console.groupEnd ()
+
+// ListCodersGate = ["Alejo Gey" , "Simon" , "Steven"]
+// console.table (ListCodersGate)
+// console.log (ListCodersGate)
