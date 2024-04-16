0a57a6a4e0a6fd4d9ede051ed70b061f24987f07
SamuVD <samuvv2905@gmail.com>
2024-04-16 03:50:40 -0500

    Se hizo una clasificación de frutas

---
+++ ClasificaciónFrutas/src/js/main.js
@@ -0,0 +1,15 @@
+//Variable que almacena las frutas
+
+let fruits = ["Green Grapes", "Green Apples", "Purple Grapes", "Red Apples"]
+
+for (let i in fruits) {
+    console.log (fruits [i])
+}
+
+//Variables de categorías
+
+const sweet = ["Red Apples", "Purple Grapes"]
+const acid = ["Green Apples", "Green Grapes"]
+
+console.table (sweet)
+console.table (acid)
