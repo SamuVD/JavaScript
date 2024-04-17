5aabbc6d0a319ccc4560818833cf696e5019829c
SamuVD <samuvv2905@gmail.com>
2024-04-17 07:08:45 -0500

    Comienzo del Manejo de Números.

---
+++ 03-ManejoDeNumbers/src/js/main.js
@@ -0,0 +1,9 @@
+//Conversión de números
+let myFirstNumber = 2
+let mySecondNumber = "2"
+document.write(myFirstNumber)
+
+console.info(myFirstNumber * mySecondNumber)
+console.info(myFirstNumber - mySecondNumber)
+console.info(myFirstNumber / mySecondNumber)
+console.info(myFirstNumber + mySecondNumber)
