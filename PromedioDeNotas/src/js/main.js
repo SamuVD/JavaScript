0ca2a6de38cbcbadb245f307246e473e66b207b3
SamuVD <samuvv2905@gmail.com>
2024-04-16 21:41:13 -0500

    Se terminó el Promedio de Notas

--- PromedioDeNotas/src/js/main.js
+++ PromedioDeNotas/src/js/main.js
@@ -1,15 +1,28 @@
 //Variables de las materias a evaluar
 
-const spanish = parseFloat (prompt ("Escriba la nota para Español: "))
-const english = parseFloat (prompt ("Escriba la nota para Inglés: "))
-const arts = parseFloat (prompt ("Escriba la nota para Artes: "))
-const deports = parseFloat (prompt ("Escriba la nota para Deportes: "))
-const sciences = parseFloat (prompt ("Escriba la nota para Ciencias: "))
-let notes = [spanish, english, arts, deports, sciences]
+const spanish = parseFloat(prompt("Escriba la nota para Español: "))
+const english = parseFloat(prompt("Escriba la nota para Inglés: "))
+const arts = parseFloat(prompt("Escriba la nota para Artes: "))
+const deports = parseFloat(prompt("Escriba la nota para Deportes: "))
+const sciences = parseFloat(prompt("Escriba la nota para Ciencias: "))
+
+let ratings = [] 
+ratings.push("Calificación de Español:", spanish,
+"Calificación de Inglés:", english,
+"Calificación de Artes:", arts,
+"Calificación de Deportes:", deports,
+"Calificación de Ciencias:", sciences
+)
+console.info(ratings)
+
+//Variable que opera 
 
 let media = (spanish + english + arts + deports + sciences) / 5
-console.warn (`Su promedio es de: ${media}`)
-// if media >=3.0 
-// console.log (`Es aprobado ${media}`)
-// else
-// console.error (`Está reprobado ${media}`)
+console.warn(`Su promedio es de: ${media}`)
+
+//Condicionales
+if (media >= 3.0 ) {
+    console.info("Aprobó el curso.")
+}   else {
+    console.error("Reprobó el curso.")
+}
