b0fe5eadc7ca4d5cfb63e99c57c0c1ec6088e7c3
SamuVD <samuvv2905@gmail.com>
2024-04-18 08:39:16 -0500

    COMIENZO CON LOS OPERADORES

---
+++ 04-ManejoDeOperadores/src/js/main.js
@@ -0,0 +1,28 @@
+//Tipos de datos básicos en Js
+
+/*
+number
+string
+boolean
+null
+undefined
+object
+function
+*/
+
+console.group("Tipos de datos Básicos en Js")
+
+//Las variables (const) siempre se deben llenar. Las (let) pueden dejar as´
+const variable1 = 12
+let variable2 = "4"
+let variable3 = "Hola mundo"
+let variable4 = true 
+let variable5 = null
+let variable6 = undefined
+let variable7 = [1,2,3,3,4,5]
+let variable8 = {"name" : "Samuel"}
+let variable9 = function name () {
+    console.log("Hola mundo")
+}
+
+console.log()
