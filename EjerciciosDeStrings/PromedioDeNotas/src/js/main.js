2ffa4ec2d77094710d6c60875288265cd965770a
SamuVD <samuvv2905@gmail.com>
2024-04-18 03:50:26 -0500

    Pulimos cositas del ejercicio Promedio de Notas

--- EjerciciosDeStrings/PromedioDeNotas/src/js/main.js
+++ EjerciciosDeStrings/PromedioDeNotas/src/js/main.js
@@ -1,28 +1,26 @@
 //Variables de las materias a evaluar
+const spanish = parseFloat(prompt("Escriba la nota para Español: "));
+const english = parseFloat(prompt("Escriba la nota para Inglés: "));
+const arts = parseFloat(prompt("Escriba la nota para Artes: "));
+const deports = parseFloat(prompt("Escriba la nota para Deportes: "));
+const sciences = parseFloat(prompt("Escriba la nota para Ciencias: "));
 
-const spanish = parseFloat(prompt("Escriba la nota para Español: "))
-const english = parseFloat(prompt("Escriba la nota para Inglés: "))
-const arts = parseFloat(prompt("Escriba la nota para Artes: "))
-const deports = parseFloat(prompt("Escriba la nota para Deportes: "))
-const sciences = parseFloat(prompt("Escriba la nota para Ciencias: "))
+let ratings = [];
+ratings.push("Español: " + " " + spanish);
+ratings.push("Inglés: " + " " + english);
+ratings.push("Artes: " + " " + arts);
+ratings.push("Deportes: " + " " + deports);
+ratings.push("Ciencias: " + " " + sciences);
 
-let ratings = [] 
-ratings.push(spanish, "Español")
-ratings.push(english, "Inglés")
-ratings.push(arts, "Artes")
-ratings.push(deports, "Deportes")
-ratings.push(sciences, "Ciencias")
+console.table(ratings);
 
-console.table(ratings)
-
-//Variable que opera 
-
-let media = (spanish + english + arts + deports + sciences) / 5
-console.warn(`Su promedio es de: ${media}`)
+//Variable que opera
+let media = (spanish + english + arts + deports + sciences) / 5;
+console.warn(`Su promedio es de: ${media}`);
 
 //Condicionales
-if (media >= 3.0 ) {
-    console.info("Aprobó el curso.")
-}   else {
-    console.error("Reprobó el curso.")
+if (media >= 3.0) {
+  console.info("Aprobó el curso.");
+} else {
+  console.error("Reprobó el curso.");
 }
