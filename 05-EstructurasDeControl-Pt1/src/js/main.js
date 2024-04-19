f762e7a5a4ca8ee711f01e2792b28e9d75598d79
SamuVD <samuvv2905@gmail.com>
2024-04-19 09:28:27 -0500

    Pusimos algunos tipos de ciclos

---
+++ 05-EstructurasDeControl-Pt1/src/js/main.js
@@ -0,0 +1,229 @@
+//Condicionales simples
+//Los condicionales (if y else) sirven para elegir entre un resultado o otro
+
+//Condicional if
+// console.group("Condicional if")
+
+// const myName = "Samuel"
+
+// if (myName == "Samuel") {
+//     console.log("Hello " + myName)
+// }
+
+// console.groupEnd()
+
+
+// Condicional if & else
+// console.group("Condicional if & else")
+
+// const ageMinForDrinkBeer = 18
+// const myAge = 18
+
+// if ((myAge < ageMinForDrinkBeer) && (myAge > 0)) {
+//     console.log("Estás cometiendo una infracción")
+// } else {
+//     console.log("Puedes beber pero cuidado")
+// }
+
+// console.groupEnd()
+
+
+// Condicional else if
+// console.group("Condicional else if")
+
+// let medioDeTransporte = prompt("¿Cómo viajas a Riwi?")
+
+// if (medioDeTransporte == "pie") {
+//     console.log("Tu viajas a pie")
+// } else if (medioDeTransporte == "bicicleta") {
+//     console.log("Tu viajas en bicicleta")
+// } else if (medioDeTransporte == "metro") {
+//     console.log("Tu viajas en metro")
+// } else if (medioDeTransporte == "moto") {
+//     console.log("Tu viajas en moto")
+// } else {console.group("Condicional if")
+
+// const myName = "Samuel"
+
+// if (myName == "Samuel") {
+//     console.log("Hello " + myName)
+// }
+
+// console.groupEnd()
+
+
+// Condicional if & else
+// console.group("Condicional if & else")
+
+// const ageMinForDrinkBeer = 18
+// const myAge = 18
+
+// if ((myAge < ageMinForDrinkBeer) && (myAge > 0)) {
+//     console.log("Estás cometiendo una infracción")
+// } else {
+//     console.log("Puedes beber pero cuidado")
+// }
+
+// console.groupEnd()
+
+
+// Condicional else if
+// console.group("Condicional else if")
+
+// let medioDeTransporte = prompt("¿Cómo viajas a Riwi?")
+
+// if (medioDeTransporte == "pie") {
+//     console.log("Tu viajas a pie")
+// } else if (medioDeTransporte == "bicicleta") {
+//     console.log("Tu viajas en bicicleta")
+// } else if (medioDeTransporte == "metro") {
+//     console.log("Tu viajas en metro")
+// } else if (medioDeTransporte == "moto") {
+//     console.log("Tu viajas en moto")
+// } else {
+//     console.log("Viajas en un medio de trasporte diferente.")
+// }
+
+// console.groupEnd()
+
+
+// Condicional switch
+// El objetivo del switch tomar una variable y preguntar si es igual a una o a varias opciones
+// console.group("Condicional switch")
+
+// switch (medioDeTransporte) {
+//     case "pie":
+//         console.log("Viajas a pie")
+//         break
+//     case "bicicleta":
+//         console.log("viajas en bicicleta")
+//         break
+//     default:
+//         console.log("viajas diferente")
+//         break
+// }
+
+// console.groupEnd()
+
+// Práctica de condicionales
+
+// const food = "supermarket"
+// let userFood = parseInt(prompt("¿escoja la opcion. 1.supermarket 2.en la calle 3.en la tienda 4.en carulla"))
+
+// if (userFood == 1) {
+//     console.error("compras tu comida en un buen lugar")
+// } else if (userFood == 2) {
+//     console.log("compras tu comida en un lugar pésimo")
+// } else if (userFood == 3) {
+//     console.log("eres un alcohólico")
+// } else if (userFood == 4) {
+//     console.log("puedes mejorar")
+// } else if (userFood == 2) {
+//     console.log("por dónde pa yo ir")
+// } else {
+//     console.log("la buena, adiós")
+// }
+// //////////////////////////////////
+// switch (food) {
+//     case "1":
+//         console.log("compras en una excelente parte")
+//         break;
+//     case "2":
+//         console.log("tienes una mala alimentación")
+//         break;
+//     case "3":
+//         console.log("compras en una excelente parte")
+//         break;
+//     case "4":
+//         console.log("tienes una mala alimentación")
+//         break;
+//     default:
+//         console.error("invalido")
+//         break;
+// }
+
+//     console.log("Viajas en un medio de trasporte diferente.")
+// }
+
+// console.groupEnd()
+
+
+// Condicional switch
+// El objetivo del switch tomar una variable y preguntar si es igual a una o a varias opciones
+// console.group("Condicional switch")
+
+// switch (medioDeTransporte) {
+//     case "pie":
+//         console.log("Viajas a pie")
+//         break
+//     case "bicicleta":
+//         console.log("viajas en bicicleta")
+//         break
+//     default:
+//         console.log("viajas diferente")
+//         break
+// }
+
+// console.groupEnd()
+
+// Práctica de condicionales
+
+// const food = "supermarket"
+// let userFood = parseInt(prompt("¿escoja la opcion. 1.supermarket 2.en la calle 3.en la tienda 4.en carulla"))
+
+// if (userFood == 1) {
+//     console.error("compras tu comida en un buen lugar")
+// } else if (userFood == 2) {
+//     console.log("compras tu comida en un lugar pésimo")
+// } else if (userFood == 3) {
+//     console.log("eres un alcohólico")
+// } else if (userFood == 4) {
+//     console.log("puedes mejorar")
+// } else if (userFood == 2) {
+//     console.log("por dónde pa yo ir")
+// } else {
+//     console.log("la buena, adiós")
+// }
+// //////////////////////////////////
+// switch (food) {
+//     case "1":
+//         console.log("compras en una excelente parte")
+//         break;
+//     case "2":
+//         console.log("tienes una mala alimentación")
+//         break;
+//     case "3":
+//         console.log("compras en una excelente parte")
+//         break;
+//     case "4":
+//         console.log("tienes una mala alimentación")
+//         break;
+//     default:
+//         console.error("invalido")
+//         break;
+// }
+
+
+//Ciclos for
+// console.group("Ciclo for")
+
+// Tabla de multiplicar del 2
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i} = ${2 * i}`)
+// }
+
+// console.groupEnd
+
+
+// Ciclo while
+// console.group("Ciclo while")
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(`2 x ${i} = ${2 * i}`)
+
+//     i++
+// }
+
+// console.groupEnd()
