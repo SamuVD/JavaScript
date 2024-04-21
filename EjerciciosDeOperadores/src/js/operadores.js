60fb72724fa987f7e9543c71c93990ede716501d
SamuVD <samuvv2905@gmail.com>
2024-04-21 18:03:30 -0500

    Se agregaron los ejercicios de Operadores

---
+++ EjerciciosDeOperadores/src/js/operadores.js
@@ -0,0 +1,129 @@
+// Exercise 1
+console.group("Exercise 1");
+
+console.info(35 > 25) //TRUE
+console.groupEnd();
+
+// Exercise 2
+console.group("Exercise 2");
+
+console.error(45 < 30) //FALSE
+console.groupEnd();
+
+// Exercise 3
+console.group("Exercise 3");
+
+console.info(50 == "50") //TRUE
+console.groupEnd();
+
+// Exercise 4
+console.group("Exercise 4");
+
+console.error("40" === 40) //FALSE
+console.groupEnd();
+
+// Exercise 5
+console.group("Exercise 5");
+
+console.info("apple" != "orange") //TRUE
+console.groupEnd();
+
+// Exercise 6
+console.group("Exercise 6");
+
+console.info("apple" !== "Apple") //TRUE
+console.groupEnd();
+
+// Exercise 7
+console.group("Exercise 7");
+
+let sinDefinir //UNDEFINED
+
+console.info(sinDefinir)
+console.groupEnd();
+
+// Exercise 8
+console.group("Exercise 8");
+
+console.info(typeof sinDefinir) //UNDEFINED
+console.groupEnd();
+
+// Exercise 9
+console.group("Exercise 9");
+
+let nullo = null //TRUE
+
+console.info(nullo == sinDefinir)
+console.groupEnd();
+
+// Exercise 10
+console.group("Exercise 10");
+
+console.error(nullo === sinDefinir) //FALSE
+console.groupEnd();
+
+// Exercise 11
+console.group("Exercise 11");
+
+console.info("2.75" > 2.5) //TRUE
+console.groupEnd();
+
+// Exercise 12
+console.group("Exercise 12");
+
+console.error("2.75" > 2.75) //FALSE
+console.groupEnd();
+
+// Exercise 13
+console.group("Exercise 13");
+
+console.info("2.75" >= 2.75) //TRUE
+console.groupEnd();
+
+// Exercise 14
+console.group("Exercise 14");
+
+console.info("2.75" <= 2.75) //TRUE
+console.groupEnd();
+
+// Exercise 15
+console.group("Exercise 15");
+
+console.error("2.75" > 2.8) //FALSE
+console.groupEnd();
+
+// Exercise 16
+console.group("Exercise 16");
+
+console.error("2.75" < 2.7) //FALSE
+console.groupEnd();
+
+// Exercise 17
+console.group("Exercise 17");
+
+console.info("John" != "john") //TRUE
+console.groupEnd();
+
+// Exercise 18
+console.group("Exercise 18");
+
+console.info("John" != "Jon") //TRUE
+console.groupEnd();
+
+// Exercise 19
+console.group("Exercise 19");
+
+console.info(30 !== "30") //TRUE
+console.groupEnd();
+
+// Exercise 20
+console.group("Exercise 20");
+
+console.info(25>>2)
+console.groupEnd();
+
+// Exercise 21
+console.group("Exercise 21");
+
+console.info(8<<3)
+console.groupEnd();
